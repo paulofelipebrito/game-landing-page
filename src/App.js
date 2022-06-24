0d971575813c24d0e6b1248f987d223823fb170f
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-06-23 22:35:35 -0300

    feat: Header component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import Header from './components/sessions/Header/Header';
 
 const App = () => (
   <div>
     <Navbar />
+    <Header />
   </div>
 );
 
