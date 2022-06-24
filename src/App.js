fb13e4c441c5b3fe292e07a5aa6a6b95633c43f8
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-06-24 07:28:37 -0300

    feat: Video section

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from './components/Header/Header';
 import AboutUs from './components/AboutUs/AboutUs';
 import SpecialMenu from './components/Menu/SpecialMenu';
 import Chef from './components/Chef/Chef';
+import Intro from './components/Intro/Intro';
 
 const App = () => (
   <div>
@@ -14,6 +15,7 @@ const App = () => (
     <AboutUs />
     <SpecialMenu />
     <Chef />
+    <Intro />
   </div>
 );
 
