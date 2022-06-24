70aaf9a3b457dd1cb00a081eaed3ae8c26c3c7bb
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-06-24 07:04:18 -0300

    feat: Menu component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Header from './components/Header/Header';
 import AboutUs from './components/AboutUs/AboutUs';
+import SpecialMenu from './components/Menu/SpecialMenu';
 
 const App = () => (
   <div>
     <Navbar />
     <Header />
     <AboutUs />
+    <SpecialMenu />
   </div>
 );
 
